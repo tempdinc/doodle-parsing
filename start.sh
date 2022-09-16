25c993ede99f8be8bd6b7656813cf7ed3f8524da
tempdinc <92944467+tempdinc@users.noreply.github.com>
2022-09-16 16:17:58 +0300

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+sudo service apache2 start
+sudo service apache2 status
+sudo service mysql start
+sudo service mysql status
+sudo service redis-server start
+sudo service redis-server status
