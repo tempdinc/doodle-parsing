1b4eba4b2feaa7b1e94a28839970ec779122f081
tempdinc <alex@tempd.live>
2022-10-05 12:30:22 +0300

    fix install.php

--- install.php
+++ install.php
@@ -20,38 +20,42 @@ error_reporting(E_ALL);
    }
 
    // Create database
-   $sql = "CREATE DATABASE parsing";
+   $sql = "CREATE DATABASE IF NOT EXISTS parsing";
    if (mysqli_query($conn, $sql)) {
       echo "Database `parsing` created successfully | ";
    } else {
       echo "Error creating database: " . mysqli_error($conn) . PHP_EOL;
    }
  
-   $sql = 'CREATE TABLE `properties` (
-      `id` bigint NOT NULL AUTO_INCREMENT,
-      `post_id` int DEFAULT NULL,
+   $sql = 'CREATE TABLE IF NOT EXISTS `properties` (
+      `id` bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
+      `is_deleted` tinyint(1) DEFAULT NULL,
+      `link` varchar(256) DEFAULT NULL,
       `address` varchar(256) DEFAULT NULL,
       `type` varchar(32) DEFAULT NULL,
       `lease_length` varchar(256) DEFAULT NULL,
       `latitude` varchar(64) DEFAULT NULL,
       `longitude` varchar(64) DEFAULT NULL,
+      `contact_phone` varchar(32) DEFAULT NULL,
+      `contact_email` varchar(128) DEFAULT NULL,
       `contact_type` varchar(64) DEFAULT NULL,
       `contact_person` varchar(256) DEFAULT NULL,
       `contact_company` varchar(256) DEFAULT NULL,
-      `contact_phone` varchar(32) DEFAULT NULL,
-      `contact_email` varchar(128) DEFAULT NULL,
       `building_units` varchar(64) DEFAULT NULL,
       `addr_line_1` varchar(128) DEFAULT NULL,
       `addr_line_2` varchar(128) DEFAULT NULL,
       `city` varchar(64) DEFAULT NULL,
       `state_cd` char(2) DEFAULT NULL,
       `zip5_cd` varchar(32) DEFAULT NULL,
-      `image_urls` text,
+      `building_name` varchar(128) DEFAULT NULL,
+      `building_desc` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci COMMENT "Unit description",
+      `property_info` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci COMMENT "Unique features",
+      `pet_policy` text,
+      `on_premise_services` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci COMMENT "Community amenities",
+      `on_premise_features` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci COMMENT "Apartment amenities",
       `listing_comments` text,
       `virtual_tour_urls` text,
-      `pet_policy` text,
       `outdoor_space` varchar(128) DEFAULT NULL,
-      `on_premise_services` text,
       `walk_score` varchar(128) DEFAULT NULL,
       `transit_score` varchar(128) DEFAULT NULL,
       `nearby_school` text,
@@ -66,22 +70,15 @@ error_reporting(E_ALL);
       `utilities_included` varchar(256) DEFAULT NULL,
       `building_security` varchar(128) DEFAULT NULL,
       `living_space` varchar(128) DEFAULT NULL,
-      `on_premise_features` text,
       `student_features` varchar(256) DEFAULT NULL,
       `kitchen` varchar(256) DEFAULT NULL,
       `parking` text,
-      `property_info` text,
       `building_features` text,
       `subdivision` varchar(32) DEFAULT NULL,
-      `building_desc` text,
-      `building_name` varchar(128) DEFAULT NULL,
       `builiding_office_hours` text,
-      `link` varchar(255) NOT NULL,
       `expences` text,
-      `is_deleted` tinyint(1) NOT NULL DEFAULT "0",
       `last_update` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-      `date_added` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-      PRIMARY KEY (id)
+      `date_added` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
     ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci';
 
    if(!mysqli_select_db($conn,'parsing')) {
@@ -96,8 +93,10 @@ error_reporting(E_ALL);
       echo "Error creating table `properties`: " . mysqli_error($conn) . PHP_EOL;
    }
 
-   $sql = 'CREATE TABLE `availability` (
-      `id` bigint NOT NULL AUTO_INCREMENT,
+   $sql = 'CREATE TABLE IF NOT EXISTS `availability` (
+      `id` bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
+      `post_id` int DEFAULT NULL,
+      `is_deleted` tinyint(1) DEFAULT NULL,
       `property_id` bigint NOT NULL,
       `bedroom_cnt` varchar(32) DEFAULT NULL,
       `bathroom_cnt` varchar(32) DEFAULT NULL,
@@ -106,8 +105,7 @@ error_reporting(E_ALL);
       `lease_length` varchar(64) DEFAULT NULL,
       `status` varchar(64) DEFAULT NULL,
       `image_urls` text,
-      `date_added` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-      PRIMARY KEY (id)
+      `date_added` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
     ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;';
 
    if(!mysqli_select_db($conn,'parsing')) {
@@ -123,4 +121,3 @@ error_reporting(E_ALL);
    }
 
    mysqli_close($conn);
-?>
