9b3598e49360029c75d0726424e58e3b753eb57c
tempdinc <alex@tempd.live>
2022-11-29 18:58:09 +0300

    fix quote-add.php

--- src/quote-add-copy.php
+++ src/quote-add-copy.php
@@ -1,5 +1,5 @@
 <?php
-
+      var_dump(json_decode('[{\"url\":\"https://drive.google.com/uc?id=1_3TKcpF6ykoeE_NCNvi-DkzLcIpBnxIJ&export=download\",\"name\":\"Unit Property Image QTE-2022-10-13\",\"extension\":\"jpg\"},{\"url\":\"https://drive.google.com/uc?id=1brQn8mpZ6OKNscB6mrX0pJkETA8xRLWa&export=download\",\"name\":\"Additional Unit Image QTE1-2022-10-13\",\"extension\":\"jpg\"},{\"url\":\"https://drive.google.com/uc?id=11OFm-jZ0uSAZ3sthSI_hfFyVChgjdWPN&export=download\",\"name\":\"Additional Unit Image QTE2-2022-10-13\",\"extension\":\"jpg\"}]'));
       $file_get = file_get_contents('https://drive.google.com/uc?id=11OFm-jZ0uSAZ3sthSI_hfFyVChgjdWPN&export=download');
       $extension = pathinfo(parse_url('https://drive.google.com/uc?id=11OFm-jZ0uSAZ3sthSI_hfFyVChgjdWPN&export=download', PHP_URL_PATH), PATHINFO_EXTENSION);
       echo $extension;
