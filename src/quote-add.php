9eba0e18477c73fb993cac81e316e4530a805a3f
tempdinc <alex@tempd.live>
2022-11-29 19:32:47 +0300

    fix quote-add.php

--- src/quote-add.php
+++ src/quote-add.php
@@ -205,7 +205,8 @@ $unit_source = 'quote';
 $wpImageArray = [];
 $decoded_image_urls = json_decode($quote_images);
 // $decoded_image_urls = $quote_images;
-// file_put_contents(LOG_DIR . '/quote-add.log', ' | value - ' . $decoded_image_urls . PHP_EOL, FILE_APPEND);
+file_put_contents(LOG_DIR . '/quote-add.log', ' | type of decoded_image_urls - ' . gettype($decoded_image_urls) . PHP_EOL, FILE_APPEND);
+file_put_contents(LOG_DIR . '/quote-add.log', ' | decoded_image_urls - ' . $decoded_image_urls . PHP_EOL, FILE_APPEND);
 if (is_array($decoded_image_urls) && count($decoded_image_urls) > 0) {
    foreach ($decoded_image_urls as $key => $value) {
       $value = json_decode($value);
