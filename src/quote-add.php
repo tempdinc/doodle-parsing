de8e3f3a23c813c2665a66dec767a25b7ee2effe
tempdinc <alex@tempd.live>
2022-12-08 17:55:14 +0300

    fix quote-add

--- src/quote-add.php
+++ src/quote-add.php
@@ -1,106 +1,4 @@
 <?php
-
-/*
-
-require_once($_SERVER['DOCUMENT_ROOT'] . '/wp-load.php');
-require_once(ABSPATH . 'wp-includes/pluggable.php');
-global $wpdb;
-
-function strtoarray($a, $t = '')
-{
-	$arr = [];
-	$a = ltrim($a, '[');
-	$a = ltrim($a, 'array(');
-	$a = rtrim($a, ']');
-	$a = rtrim($a, ')');
-	$tmpArr = explode(",", $a);
-	foreach ($tmpArr as $v) {
-		if ($t == 'keys') {
-			$tmp = explode("=>", $v);
-			$k = $tmp[0];
-			$nv = $tmp[1];
-			$k = trim(trim($k), "'");
-			$k = trim(trim($k), '"');
-			$nv = trim(trim($nv), "'");
-			$nv = trim(trim($nv), '"');
-			$arr[$k] = $nv;
-		} else {
-			$v = trim(trim($v), "'");
-			$v = trim(trim($v), '"');
-			$arr[] = $v;
-		}
-	}
-	return $arr;
-}
-
-//Save logs
-$log  = json_encode($_POST) . PHP_EOL . "-------------------------" . PHP_EOL;
-//Save string to log, use FILE_APPEND to append.
-$link = $_SERVER['DOCUMENT_ROOT'] . '/wp-content/themes/brikk-child/logs_reservation.txt';
-file_put_contents($link, $log, FILE_APPEND);
-
-if (isset($_POST['quote_id'])) {
-	$q_id = $_POST['quote_id'];
-	$q_data = $wpdb->get_results("SELECT * FROM quotes_table as qt WHERE qt.quote_id = '$q_id'");
-
-	if ($wpdb->num_rows) {
-		$existing_link = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['SERVER_NAME'] . '/pre-booking/?pid=' . base64_encode($q_data[0]->id);
-		$response = ['status_code' => 400, 'message' => 'Quote already exists!', 'existing_link' => $existing_link];
-		echo json_encode($response);
-		exit();
-	}
-}
-
-$quote = null;
-$quote_id = null;
-if (isset($_POST['quote_id']) && isset($_POST['quote_title']) && isset($_POST['quote_address'])) {
-	$quote = $wpdb->insert('quotes_table', array(
-		'quote_id' => $_POST['quote_id'],
-		'quote_title' => ((isset($_POST['quote_title'])) ? $_POST['quote_title'] : ''),
-		'quote_description' => ((isset($_POST['quote_description'])) ? $_POST['quote_description'] : ''),
-		'quote_address' => ((isset($_POST['quote_address'])) ? $_POST['quote_address'] : ''),
-		'quote_street' => ((isset($_POST['quote_street'])) ? $_POST['quote_street'] : ''),
-		'quote_city' => ((isset($_POST['quote_city'])) ? $_POST['quote_city'] : ''),
-		'quote_state' => ((isset($_POST['quote_state'])) ? $_POST['quote_state'] : ''),
-		'quote_zip' => ((isset($_POST['quote_zip'])) ? $_POST['quote_zip'] : ''),
-		'quote_baths' => ((isset($_POST['quote_baths'])) ? $_POST['quote_baths'] : ''),
-		'quote_beds' => ((isset($_POST['quote_beds'])) ? $_POST['quote_beds'] : ''),
-		'quote_sqft' => ((isset($_POST['quote_sqft'])) ? $_POST['quote_sqft'] : ''),
-		'quote_link' => ((isset($_POST['quote_link'])) ? $_POST['quote_link'] : '')
-	));
-	$quote_id = $wpdb->insert_id;
-} else {
-	$response = ['status_code' => 400, 'message' => 'These fields are required: "quote_id","quote_title" and "quote_address"!'];
-	echo json_encode($response);
-	exit();
-}
-
-$response = [];
-
-if (isset($quote_id) && $_POST['images'] != "[]") {
-	$images = strtoarray($_POST['images']);
-	$data_to_save = '';
-	foreach ($images as $img) {
-		$img = ltrim($img, '\\"');
-		$img = rtrim($img, '\\');
-		if ($img != '')
-			$quote = $wpdb->insert(
-				'quote_attachments',
-				array(
-					'quote_table_id' => $quote_id,
-					'attachment_path' => $img
-				)
-			);
-	}
-
-	$response = ['status_code' => 200, 'pre_booking_page_link' => $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['SERVER_NAME'] . '/pre-booking/?pid=' . base64_encode($quote_id)];
-} else {
-	$response = ['status_code' => 400, 'message' => 'An error occurred, try again later!'];
-}
-
-echo json_encode($response);
-*/
-
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
@@ -108,12 +6,13 @@ error_reporting(E_ALL);
 use App\Classes\MySQL;
 
 require_once 'bootstrap.php';
-//require_once($_SERVER['DOCUMENT_ROOT'] . '/wp-load.php');
 require_once(realpath('../../wp-load.php'));
 
 // Clear log files
 $f = fopen(LOG_DIR . '/quote-add.log', 'w');
 fclose($f);
+
+file_put_contents(LOG_DIR . '/quote-add.log', '[' . date('Y-m-d H:i:s') . ']  Start >>> ' . PHP_EOL, FILE_APPEND);
 
 if (isset($_POST['quote_id']) && isset($_POST['quote_title']) && isset($_POST['quote_address'])) {
    $quote_id = $_POST['quote_id'];
@@ -142,19 +41,17 @@ if (isset($_POST['quote_id']) && isset($_POST['quote_title']) && isset($_POST['q
    $region_slug = str_replace(' ', '-', $city_low . ' ' . $state_low);
    $key = array_search($region_slug, array_column($rz_full_regions, 'slug'));
    $rz_region_id = $rz_full_regions[$key]['term_id'];
-   echo 'rz_region_id - ' . $rz_region_id;
    $custom_tax = array(
       'rz_regions' => array(
          $rz_region_id
       )
    );
-
-   file_put_contents(LOG_DIR . '/quote-add.log', '[' . date('Y-m-d H:i:s') . ']  Start >>> ' . PHP_EOL, FILE_APPEND);
-   file_put_contents(LOG_DIR . '/quote-add.log', $quote_id . ' > ' . $quote_title . ' > ' . $quote_description . ' > ', FILE_APPEND);
-   file_put_contents(LOG_DIR . '/quote-add.log', $quote_address . ' > ' . $quote_street . ' > ' . $quote_city . ' > ' . $quote_state . ' > ', FILE_APPEND);
+   file_put_contents(LOG_DIR . '/quote-add.log', $quote_id . ' > ' . $quote_title . ' > ' . $quote_description . PHP_EOL, FILE_APPEND);
+   file_put_contents(LOG_DIR . '/quote-add.log', $quote_address . ' > ' . $quote_street . ' > ' . $quote_city . ' > ' . $quote_state . PHP_EOL, FILE_APPEND);
    file_put_contents(LOG_DIR . '/quote-add.log', $quote_zip . ' > ' . $quote_baths . ' > ' . $quote_beds . ' > ' . $quote_sqft . ' > ' . $quote_images . PHP_EOL, FILE_APPEND);
 } else {
    $response = ['status_code' => 400, 'message' => 'These fields are required: "quote_id","quote_title" and "quote_address"!'];
+   file_put_contents(LOG_DIR . '/quote-add.log', ' > ' . json_encode($response) . PHP_EOL, FILE_APPEND);
    echo json_encode($response);
    exit();
 }
@@ -206,24 +103,17 @@ $unit_source = 'quote';
 // Image gallery
 $is_gallery_empty = true;
 $wpImageArray = [];
-// $image_urls = $property->image_urls;
-// $decoded_image_urls = json_decode('"' . $quote_images . '"');
 $decoded_image_urls = json_decode(json_decode('"' . $quote_images . '"', true));
 file_put_contents(LOG_DIR . '/quote-add.log', ' | type of decoded_image_urls - ' . $decoded_image_urls . PHP_EOL, FILE_APPEND);
 if (is_array($decoded_image_urls) && count($decoded_image_urls) > 0) {
    foreach ($decoded_image_urls as $key => $value) {
-      // $value = json_decode($value);
-      // file_put_contents(LOG_DIR . '/quote-add.log', ' | value - ' . $value->name . ' | ' . str_replace(' ', '_', strtolower($value->name)) . ' | ' . $value->url . ' | ' . $value->extension . PHP_EOL, FILE_APPEND);
-      echo ' | value - ' . $value->name . ' | ' . str_replace(' ', '_', strtolower($value->name)) . ' | ' . $value->url . ' | ' . $value->extension . PHP_EOL;
+      file_put_contents(LOG_DIR . '/quote-add.log', ' | value - ' . $value->name . ' | ' . str_replace(' ', '_', strtolower($value->name)) . ' | ' . $value->url . ' | ' . $value->extension . PHP_EOL, FILE_APPEND);
       $re = '`^.*/`m';
       $subst = '';
       // IMAGE NAME CHECKING
       $orig_filename = $value->name;
-      // echo ' | orig_full_filename - ' . $orig_full_filename;
       $orig_fileextension = $value->extension;
-      // echo ' | orig_fileextension - ' . $orig_fileextension; 
       $orig_filename = str_replace(' ', '_', strtolower($orig_filename));
-      // echo ' | ' . $orig_filename;
       $filename_path = __DIR__ . '/images' . '/' . $orig_filename . '.' . $orig_fileextension;
       $is_file_exist = file_exists($filename_path);
 
@@ -244,7 +134,6 @@ if (is_array($decoded_image_urls) && count($decoded_image_urls) > 0) {
 }
 file_put_contents(LOG_DIR . '/quote-add.log', ' | WP Image ID - END' . PHP_EOL, FILE_APPEND);
 
-// exit();
 // Checking for images of post
 $is_gallery_empty = (count($wpImageArray) == 0) ? true : false;
 $rz_gallery = json_encode($wpImageArray);
@@ -271,8 +160,6 @@ if ($main_post_insert_result && $main_post_insert_result != 0) {
       'rz_booking_type'             => 'Request booking',
       'rz_city'                     => $quote_city,
       'rz_listing_type'             => '25769',
-      'rz_location'                 => '',
-      'rz_location'                 => '',
       'rz_location'                 => $np_rz_location__address,
       'rz_location__address'        => $np_rz_location__address,
       'rz_location__lat'            => '',
@@ -315,15 +202,22 @@ if (!$is_gallery_empty) {
    $rz_search = 1;
    $rz_multi_units = [];
 
-   echo ' | rz_unit_type - ' . $rz_unit_type . ' | property address - ' . $property_address . ' | main_post_insert_result - ' . $main_post_insert_result . ' | property->id - ' . $property->id . PHP_EOL;
-   file_put_contents(LOG_DIR . '/quote-add.log', ' | rz_unit_type - ' . $rz_unit_type . ' | property address - ' . $property_address . ' | main_post_insert_result - ' . $main_post_insert_result . ' | property->id - ' . $property->id . PHP_EOL, FILE_APPEND);
-
    if ($main_post_insert_result && $main_post_insert_result != 0) {
 
       $new_property_meta = [];
 
+      $quote_baths = floatval($quote_baths);
+      if ($quote_baths == intval($quote_baths)) {
+         $quote_baths = round($quote_baths);
+      } else {
+         $quote_baths = round($quote_baths, 1);
+      }
       $bath_count = trim(preg_replace("/[a-zA-Z]/", "", $quote_baths)); // rz_bathrooms
+
+      $quote_beds = round(floatval($quote_beds));
       $bed_count = trim(preg_replace("/[a-zA-Z]/", "", $quote_beds)); // rz_bed
+
+      $quote_sqft = round(floatval($quote_sqft));
       $sqft = trim(preg_replace("/\D/", "", $quote_sqft)); // rz_sqft
 
       $listing_price = clearPrice(100);
@@ -351,10 +245,9 @@ if (!$is_gallery_empty) {
       }
    }
 } else {
-   echo 'No images for post';
+   file_put_contents(LOG_DIR . '/quote-add.log', ' | No images for post | ' . PHP_EOL, FILE_APPEND);
 }
 
-echo " >>> " . date("Y-m-d H:i:s") . " - End.." . PHP_EOL;
 file_put_contents(LOG_DIR . '/quote-add.log', ' >>> [' . date('Y-m-d H:i:s') . '] - End..' . PHP_EOL, FILE_APPEND);
 
 // Stop signals handler
@@ -461,7 +354,7 @@ function moveToWp($image_url, $alt_text, $unit_source)
       update_post_meta($attach_id, '_wp_attachment_image_alt', $alt_text);
       return $attach_id;
    } catch (Exception $e) {
-      echo ($e->getMessage());
+      file_put_contents(LOG_DIR . '/quote-add.log', ' > MoveToWP error - ' . $e->getMessage() . PHP_EOL, FILE_APPEND);
       return false;
    }
 }
@@ -477,7 +370,6 @@ function clearImages()
 
 function checkSlug($slug_string)
 {
-   echo $slug_string . PHP_EOL;
    $wp_db = new MySQL('wp', 'local');
    $query = $wp_db->pdo->prepare("SELECT count(*) FROM `wp_terms` WHERE `slug` = ? LIMIT 1");
    $query->execute([$slug_string]);
