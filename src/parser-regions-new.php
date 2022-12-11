11b0e5cd60d129053e07771c5c538398cbdba34e
tempdinc <alex@tempd.live>
2022-12-11 16:40:10 +0300

    fix quote add

--- src/parser-regions-new.php
+++ src/parser-regions-new.php
@@ -6,32 +6,30 @@ error_reporting(E_ALL);
 require_once __DIR__ . '/bootstrap.php';
 require_once(realpath('../../wp-load.php'));
 
-echo date("Y-m-d H:i:s") . "Parse regions - ";
-file_put_contents(LOG_DIR . '/parse-regions.log', '[' . date('Y-m-d H:i:s') . '] Parse regions - ', FILE_APPEND);
+file_put_contents(LOG_DIR . '/parser-regions-new.log', '[' . date('Y-m-d H:i:s') . '] Parse regions start >>> ' . PHP_EOL, FILE_APPEND);
 
 // NEW FULL REGIONS
+/*
 $f = fopen(LOG_DIR . '/new-regions.json', 'w');
 fclose($f);
-
+*/
 // Checking TERMS rz_regions for existing regions from cities.json
 $citiesDB = file_get_contents(__DIR__ . '/cities.json');
 $citiesDB = json_decode($citiesDB, true);
-$counter = 0;
 foreach ($citiesDB as $states) {
     foreach ($states as $code => $citiesArray) {
         foreach ($citiesArray as $city) {
             $region = strtoupper($city) . ', ' . strtoupper($code);
             $region_slug = str_replace(' ', '-', preg_replace('/[^ a-z\d]/ui', '', strtolower($region)));
-            echo ' | region_slug - ' . $region_slug;
+            file_put_contents(LOG_DIR . '/parser-regions-new.log', ' | ' . $region . ' > ' . $region_slug, FILE_APPEND);
             $region_term = get_term_by('slug', $region_slug, 'rz_regions');
-            var_dump($region_slug);
             if ($region_term === false) {
                 $rz_region_id = insertNewTerm($region, $region_slug);
             }
         }
     }
 }
-// exit();
+file_put_contents(LOG_DIR . '/parser-regions-new.log', PHP_EOL, FILE_APPEND);
 
 $old_regions = [];
 
@@ -69,21 +67,16 @@ foreach ($full_regions as $full_region => $full_cities) {
     }
 }
 file_put_contents('old-regions.json', json_encode($old_regions));
-// unset($old_regions);
-// exit();
 
 $citiesDB = file_get_contents(__DIR__ . '/cities.json');
 $citiesDB = json_decode($citiesDB, true);
-$counter = 0;
 foreach ($citiesDB as $states) {
     foreach ($states as $code => $citiesArray) {
         foreach ($citiesArray as $city) {
             unset($marketDB);
             $region = strtoupper($city) . ', ' . strtoupper($code);
             $region_slug = str_replace(' ', '-', preg_replace('/[^ a-z\d]/ui', '', strtolower($region)));
-
             $key = array_search($region, $old_regions_key);
-
             $city = str_replace(' ', '-', strtolower($city));
             $code = strtolower($code);
             echo $city . '-' . $code . PHP_EOL;
@@ -98,16 +91,13 @@ foreach ($citiesDB as $states) {
                     foreach ($markets as $market) {
                         $new_city = $market['city'] . ', ' . $market['state'];
                         $new_city_slug = str_replace(' ', '-', preg_replace('/[^ a-z\d]/ui', '', strtolower($new_city)));
-
                         $new_cities[] = [
                             'city_name' => $new_city,
                             'city_slug' => $new_city_slug
                         ];
-                        $counter++;
                     }
                 }
             }
-
             if ($key !== false && $old_regions[$region] !== NULL) {
                 $new_cities = array_merge($old_regions[$region], $new_cities);
                 $new_cities = array_unique($new_cities, SORT_REGULAR);
@@ -129,24 +119,26 @@ foreach ($full_regions as $full_region => $full_cities) {
     }
 }
 
+$counter = 0;
 $rz_full_regions = [];
 foreach ($old_regions as $full_region => $full_cities) {
     $region_slug = str_replace(' ', '-', preg_replace('/[^ a-z\d]/ui', '', strtolower($full_region)));
     foreach ($full_cities as $full_city) {
-        // $region_slug
         $rz_full_regions[] = [
             'city_name'     => strtoupper($full_city['city_name']),
             'city_slug'     => $full_city['city_slug'],
             'region_name'   => $full_region,
             'region_slug'   => $region_slug
         ];
+        $counter++;
     }
 }
 file_put_contents('new-regions.json', json_encode($old_regions));
 file_put_contents('new-cities.json', json_encode($rz_full_regions));
 
-file_put_contents(LOG_DIR . '/parse-regions.log', ' total added cities - ' . $counter . PHP_EOL . ' END >>> ' . date('Y-m-d H:i:s'), FILE_APPEND);
+file_put_contents(LOG_DIR . '/parser-regions-new.log', ' total added cities - ' . $counter . PHP_EOL . ' END >>> ' . date('Y-m-d H:i:s'), FILE_APPEND);
 // NEW FULL REGIONS END
+
 function insertNewTerm($region_name, $region_slug)
 {
     $insert_res = wp_insert_term(
@@ -158,11 +150,12 @@ function insertNewTerm($region_name, $region_slug)
             'parent'      => 0
         )
     );
-
     if (is_wp_error($insert_res)) {
+        file_put_contents(LOG_DIR . '/parser-regions-new.log', ' | ERROR ADDING NEW REGION: ' . $insert_res->get_error_message(), FILE_APPEND);
         echo $insert_res->get_error_message();
         return false;
     } else {
+        file_put_contents(LOG_DIR . '/parser-regions-new.log', ' | NEW REGION: ' . $region_name . ' > ' . $region_slug, FILE_APPEND);
         return $insert_res['term_id'];
     }
 }
