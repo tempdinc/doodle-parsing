c76c11312939513701da9d799714f13e6d6e7225
tempdinc <alex@tempd.live>
2022-12-13 19:19:05 +0300

    fix regions

--- src/fix-post-regions-postmeta.php
+++ src/fix-post-regions-postmeta.php
@@ -12,95 +12,57 @@ require_once(realpath('../../wp-load.php'));
 $f = fopen(LOG_DIR . '/fix-post-regions.log', 'w');
 fclose($f);
 
-echo date("Y-m-d H:i:s") . " Transfer regions - ";
-file_put_contents(LOG_DIR . '/fix-post-regions.log', '[' . date('Y-m-d H:i:s') . '] Transfer regions - ', FILE_APPEND);
+echo date("Y-m-d H:i:s") . " Start fix regions - ";
+file_put_contents(LOG_DIR . '/fix-post-regions.log', '[' . date('Y-m-d H:i:s') . '] Transfer regions | ', FILE_APPEND);
 
-$citiesDB = file_get_contents(__DIR__ . '/cities.json');
-$citiesDB = json_decode($citiesDB, true);
-$regions = [];
-
-foreach ($citiesDB as $states) {
-    foreach ($states as $code => $citiesArray) {
-        foreach ($citiesArray as $city) {
-            $city_new = str_replace(' ', '-', strtolower($city));
-            $code_new = str_replace(' ', '-', strtolower($code));
-            $city_up = strtoupper($city);
-            $code_up = strtoupper($code);
-            $regions[] = [
-                'name' => $city_up . ', ' . $code_up,
-                'slug' => $city_new . '-' . $code_new
-            ];
-        }
-    }
-}
-
-foreach ($regions as $region) {
-    $result = insertNewTerm($region['name'], $region['slug']);
-    echo $result;
-    if ($result) {
-        file_put_contents(LOG_DIR . '/fix-post-regions.log', ' region - ' . $region['name'] . ' | ' . $region['slug'] . PHP_EOL, FILE_APPEND);
-    } else {
-        file_put_contents(LOG_DIR . '/fix-post-regions.log', ' region - ' . $region['name'] . ' | ' . $region['slug'] . ' - NOT ADDED!!!' . PHP_EOL, FILE_APPEND);
-    }
-}
-
-file_put_contents(LOG_DIR . '/fix-post-regions.log', ' >>> [' . date('Y-m-d H:i:s') . '] - End..' . PHP_EOL, FILE_APPEND);
-
-// Start transfer
-echo date("Y-m-d H:i:s") . " Start fixing post regions ";
-file_put_contents(LOG_DIR . '/fix-post-regions.log', '[' . date('Y-m-d H:i:s') . ']  Start >>> ' . PHP_EOL, FILE_APPEND);
-
+// NEW REGIONS START
 $terms = get_terms([
-    'taxonomy' => 'rz_regions',
-    'hide_empty' => false,
+    'taxonomy'  => 'rz_regions',
+    'hide_empty' => false
 ]);
 
 $rz_regions = [];
 foreach ($terms as $term) {
     $rz_regions[] = [
-        'term_id' => $term->term_id,
-        'name' => $term->name,
-        'slug' => $term->slug
+        'term_id'   => $term->term_id,
+        'name'      => $term->name,
+        'slug'      => $term->slug
     ];
 }
-// var_dump($rz_regions);
-// exit();
 
-$regionsDB = file_get_contents(__DIR__ . '/regions.json');
-$regionsDB = json_decode($regionsDB, true);
-
+$rz_full_cities = file_get_contents(__DIR__ . '/new-cities.json');
+$rz_full_cities = json_decode($rz_full_cities, true);
 $rz_full_regions = [];
-foreach ($regionsDB as $regionDB) {
-    // var_dump($value);
-    foreach ($regionDB as $region_key => $region_cities) {
-        // var_dump($city_key);
-        $rz_regions_key = array_search($region_key, array_column($rz_regions, 'name'));
+foreach ($rz_full_cities as $rz_full_city) {
+    $rz_regions_key = array_search($rz_full_city['region_slug'], array_column($rz_regions, 'slug'));
+    if ($rz_regions_key !== false) {
         $term_id = $rz_regions[$rz_regions_key]['term_id'];
-        $term_name = $rz_regions[$rz_regions_key]['name'];
-        $term_slug = $rz_regions[$rz_regions_key]['slug'];
-        // var_dump($term_id);
-        foreach ($region_cities as $region_city) {
-            $region_city_up = strtoupper($region_city);
-            $rz_full_regions[] = [
-                'term_id' => $term_id,
-                'name' => $region_city_up,
-                'slug' => $term_slug
-            ];
-            file_put_contents(LOG_DIR . '/fix-post-regions.log', ' >>> [' . $term_id . '] - ' . $term_name . ' | ' . $region_city_up . ' | ' . $term_slug . PHP_EOL, FILE_APPEND);
-        }
+        // $term_name = $rz_regions[$rz_regions_key]['name'];
+        // $term_slug = $rz_regions[$rz_regions_key]['slug'];
+        $rz_full_regions[] = [
+            'term_id'       => $term_id,
+            'city_name'     => $rz_full_city['city_name'],
+            'city_slug'     => $rz_full_city['city_slug'],
+            'region_name'   => $rz_full_city['region_name'],
+            'region_slug'   => $rz_full_city['region_slug']
+        ];
     }
 }
-// var_dump($rz_full_regions);
-// exit();
+unset($rz_regions);
+unset($rz_full_cities);
+// NEW REGIONS END
 
 $wp_db = new MySQL('wp', 'local');
 $query = $wp_db->pdo->prepare("SELECT count(*) FROM `wp_posts` WHERE `post_type` = ? LIMIT 1");
 $query->execute(['rz_listing']);
 $total_rz_listing = $query->fetchColumn();
-$pages = intdiv($total_rz_listing, 100);
+$pages = intdiv($total_rz_listing, 500);
+file_put_contents(LOG_DIR . '/fix-post-regions.log', ' Total posts - ' . $total_rz_listing . PHP_EOL, FILE_APPEND);
 echo 'Total posts - ' . $total_rz_listing . PHP_EOL;
 for ($i = 0; $i <= $pages; $i++) {
-    $posts = $wp_db->getAllPostsRZListing($i * 100, 100);
+    echo ' | page #' . $i . PHP_EOL;
+    unset($posts);
+    $posts = $wp_db->getAllPostsRZListing($i * 500, 500);
     foreach ($posts as $post) {
         $city_meta = $wp_db->getAllMetaByPostByMetakey($post->id, 'rz_city');
         if (is_array($city_meta)) {
@@ -114,29 +76,37 @@ for ($i = 0; $i <= $pages; $i++) {
         } else {
             $state_meta = $state_meta;
         }
-        $city_new = trim(strtoupper($city_meta));
-        $code_new = trim(strtoupper($state_meta));
-        $city_name = $city_new . ', ' . $code_new;
-        file_put_contents(LOG_DIR . '/fix-post-regions.log', ' [' . $post->id . '] | ' . $city_name . ' | ', FILE_APPEND);
-        $key = array_search($city_name, array_column($rz_full_regions, 'name'));
+        $city_slug = str_replace(' ', '-', preg_replace('/[^ a-z\d]/ui', '', strtolower($city_meta . ' ' . $state_meta)));
+        unset($city_meta);
+        unset($state_meta);
+        $key = array_search($city_slug, array_column($rz_full_regions, 'city_slug'));
         if ($key !== false) {
-            echo $post->id;
-            echo ' | ' . $city_name;
-            echo ' | ' . $rz_full_regions[$key]['name'];
-            file_put_contents(LOG_DIR . '/fix-post-regions.log', $rz_full_regions[$key]['name'] . ' | ' . $rz_full_regions[$key]['term_id'], FILE_APPEND);
-            wp_set_post_terms($post->id, [$rz_full_regions[$key]['term_id']], 'rz_regions');
-            // $postmeta_response = add_post_meta($post->id,'rz_listing_region', $rz_full_regions[$key]['slug'], true);
-            $postmeta_response = update_post_meta($post->id, 'rz_listing_region', $rz_full_regions[$key]['slug']);
-            if (!$postmeta_response) {
-                $postmeta_response = add_post_meta($post->id, 'rz_listing_region', $rz_full_regions[$key]['slug'], true);
-            }
-            // $postmeta_response = addPostMeta($post->id, 'rz_listing_region', $rz_full_regions[$key]['slug'], true);
-            if ($postmeta_response !== false) {
-                echo ' - OK ';
+            $rz_region_id = $rz_full_regions[$key]['term_id'];
+        } else {
+            $undefined_region = get_term_by('slug', 'undefined', 'rz_regions');
+            if ($undefined_region !== false) {
+                $rz_region_id = $undefined_region->term_id;
             } else {
-                echo " \033[34m - NOT OK\033[0m ";
+                $rz_region_id = insertNewTerm('UNDEFINED REGION', 'undefined');
             }
         }
+        $term_region = get_term_by('term_id', $rz_region_id, 'rz_regions');
+        $region_slug = $term_region->slug;
+        $term_region_id = (int)$rz_region_id;
+
+        wp_set_post_terms($post->id, [$term_region_id], 'rz_regions');
+        // $postmeta_response = add_post_meta($post->id,'rz_listing_region', $rz_full_regions[$key]['slug'], true);
+        $postmeta_response = update_post_meta($post->id, 'rz_listing_region', $region_slug);
+        if ($postmeta_response === false) {
+            $postmeta_response = add_post_meta($post->id, 'rz_listing_region', $region_slug, true);
+        }
+        // $postmeta_response = addPostMeta($post->id, 'rz_listing_region', $rz_full_regions[$key]['slug'], true);
+        if ($postmeta_response !== false) {
+            file_put_contents(LOG_DIR . '/fix-post-regions.log', ' [' . $post->id . '] | ' . $city_slug . ' | ' . $region_slug . ' - OK!', FILE_APPEND);
+        } else {
+            file_put_contents(LOG_DIR . '/fix-post-regions.log', ' [' . $post->id . '] | ' . $city_slug . ' | ' . $region_slug . ' - NOT OK!', FILE_APPEND);
+        }
+        /*
         $rz_listing_type = $wp_db->getAllMetaByPostByMetakey($post->id, 'rz_listing_type');
         if ($rz_listing_type == '380' || $rz_listing_type == 380) {
             $postmeta_response = update_post_meta($post->id, 'rz_ranking', '4');
@@ -152,11 +122,11 @@ for ($i = 0; $i <= $pages; $i++) {
                 $postmeta_response = add_post_meta($post->id, 'rz_unit_type', 'single', true);
             }
         }
-        echo PHP_EOL;
+*/
+        file_put_contents(LOG_DIR . '/fix-post-regions.log',  ' | ' . round(memory_get_usage() / 1048576, 2) . '' . ' MB', FILE_APPEND);
         file_put_contents(LOG_DIR . '/fix-post-regions.log', PHP_EOL, FILE_APPEND);
     }
 }
-
 echo " >>> " . date("Y-m-d H:i:s") . " - End.." . PHP_EOL;
 file_put_contents(LOG_DIR . '/fix-post-regions.log', ' >>> [' . date('Y-m-d H:i:s') . '] - End..' . PHP_EOL, FILE_APPEND);
 
