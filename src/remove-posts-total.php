70ee09f5ad4c60ff3e1adebef21d184275134f86
tempdinc <alex@tempd.live>
2022-12-27 22:30:43 +0300

    fix removeposts-total.php

--- src/remove-posts-total.php
+++ src/remove-posts-total.php
@@ -48,7 +48,6 @@ file_put_contents(LOG_DIR . '/remove-posts-total.log', PHP_EOL . '[' . date('Y-m
 $query = $parsing_db->pdo->prepare("SELECT count(*) FROM `properties` WHERE `post_id` IS NOT NULL AND `post_id` != '' LIMIT 1");
 $query->execute();
 $total_properties = $query->fetchColumn();
-echo 'Total properties WHERE post_id IS NOT NULL - ' . $total_properties . PHP_EOL;
 file_put_contents(LOG_DIR . '/remove-posts-total.log', "Total properties WHERE post_id IS NOT NULL && post_id != '' - " . $total_properties . PHP_EOL, FILE_APPEND);
 $pages = intdiv($total_properties, 100);
 for ($i = 0; $i <= $pages; $i++) {
@@ -64,7 +63,6 @@ for ($i = 0; $i <= $pages; $i++) {
 }
 $property_existing_posts = array_unique($property_existing_posts);
 sort($property_existing_posts);
-echo 'Properties existing posts unique - ' . count($property_existing_posts) . PHP_EOL;
 file_put_contents(LOG_DIR . '/remove-posts-total.log', 'Properties existing posts unique - ' . count($property_existing_posts) . PHP_EOL, FILE_APPEND);
 $existing_posts = array_merge($existing_posts, $property_existing_posts);
 
@@ -74,7 +72,6 @@ file_put_contents(LOG_DIR . '/remove-posts-total.log', PHP_EOL . '[' . date('Y-m
 $query = $parsing_db->pdo->prepare("SELECT count(*) FROM `availability` WHERE `post_id` IS NOT NULL AND `post_id` != '' LIMIT 1");
 $query->execute();
 $total_availabilities = $query->fetchColumn();
-echo 'Total availabilities WHERE post_id IS NOT NULL - ' . $total_availabilities . PHP_EOL;
 file_put_contents(LOG_DIR . '/remove-posts-total.log', "Total availabilities WHERE post_id IS NOT NULL && post_id != '' - " . $total_availabilities . PHP_EOL, FILE_APPEND);
 $pages = intdiv($total_availabilities, 100);
 for ($i = 0; $i <= $pages; $i++) {
@@ -90,13 +87,11 @@ for ($i = 0; $i <= $pages; $i++) {
 }
 $availability_existing_posts = array_unique($availability_existing_posts);
 sort($availability_existing_posts);
-echo 'Availability existing posts unique - ' . count($availability_existing_posts) . PHP_EOL;
 file_put_contents(LOG_DIR . '/remove-posts-total.log', 'Availability existing posts unique - ' . count($availability_existing_posts) . PHP_EOL, FILE_APPEND);
 
 
 $existing_posts = array_unique(array_merge($existing_posts, $availability_existing_posts));
 sort($existing_posts);
-echo 'Total (availability & property) existing posts unique - ' . count($existing_posts) . PHP_EOL;
 file_put_contents(LOG_DIR . '/remove-posts-total.log', PHP_EOL . 'Total (availability & property) existing posts unique - ' . count($existing_posts) . PHP_EOL, FILE_APPEND);
 
 
@@ -107,8 +102,7 @@ $property_existing_posts_to_remove = $property_existing_posts;
 $listing_type = '25769';
 $wp_db = new MySQL('wp', 'local');
 $total_posts = $wp_db->countPostsRZListing($listing_type);
-echo 'Total post ids with listing type ' . $listing_type . ': ' . $total_posts . PHP_EOL;
-file_put_contents(LOG_DIR . '/remove-posts-total.log', PHP_EOL . 'Total post ids with listing type ' . $listing_type . ': ' . $total_posts . PHP_EOL, FILE_APPEND);
+file_put_contents(LOG_DIR . '/remove-posts-total.log', 'Total post ids with listing type ' . $listing_type . ': ' . $total_posts . PHP_EOL, FILE_APPEND);
 $limit = 100;
 $pages = intdiv($total_posts, $limit);
 $total_removed_posts = 0;
@@ -128,14 +122,14 @@ for ($i = 0; $i <= $pages; $i++) {
 }
 $posts_to_remove = array_unique($posts_to_remove);
 sort($posts_to_remove);
-file_put_contents(LOG_DIR . '/remove-posts-total.log', PHP_EOL . 'Posts to remove from wp_posts table - ' . count($posts_to_remove) . PHP_EOL, FILE_APPEND);
+file_put_contents(LOG_DIR . '/remove-posts-total.log', 'Posts to remove from wp_posts table - ' . count($posts_to_remove) . PHP_EOL, FILE_APPEND);
 
 
 // 4. Removing availability records with NON existing post_ids
-file_put_contents(LOG_DIR . '/remove-posts-total.log', '[' . date('Y-m-d H:i:s') . '] 4. Removing availability records with NON existing post_ids' . PHP_EOL, FILE_APPEND);
+file_put_contents(LOG_DIR . '/remove-posts-total.log', PHP_EOL . '[' . date('Y-m-d H:i:s') . '] 4. Removing availability records with NON existing post_ids' . PHP_EOL, FILE_APPEND);
 $availability_existing_posts_to_remove = array_unique($availability_existing_posts_to_remove);
 sort($availability_existing_posts_to_remove);
-file_put_contents(LOG_DIR . '/remove-posts-total.log', PHP_EOL . 'Posts to remove from availability table - ' . count($availability_existing_posts_to_remove) . PHP_EOL, FILE_APPEND);
+file_put_contents(LOG_DIR . '/remove-posts-total.log', 'Posts to remove from availability table - ' . count($availability_existing_posts_to_remove) . PHP_EOL, FILE_APPEND);
 foreach ($availability_existing_posts_to_remove as $post_id) {
     $sql_query = "DELETE FROM `availability` WHERE `post_id` = ?";
     $query = $parsing_db->pdo->prepare($sql_query);
@@ -145,10 +139,10 @@ foreach ($availability_existing_posts_to_remove as $post_id) {
 
 
 // 5. Removing properties records with NON existing post_ids
-file_put_contents(LOG_DIR . '/remove-posts-total.log', '[' . date('Y-m-d H:i:s') . '] 5. Removing properties records with NON existing post_ids' . PHP_EOL, FILE_APPEND);
+file_put_contents(LOG_DIR . '/remove-posts-total.log', PHP_EOL . '[' . date('Y-m-d H:i:s') . '] 5. Removing properties records with NON existing post_ids' . PHP_EOL, FILE_APPEND);
 $property_existing_posts_to_remove = array_unique($property_existing_posts_to_remove);
 sort($property_existing_posts_to_remove);
-file_put_contents(LOG_DIR . '/remove-posts-total.log', PHP_EOL . 'Posts to remove from properties table - ' . count($property_existing_posts_to_remove) . PHP_EOL, FILE_APPEND);
+file_put_contents(LOG_DIR . '/remove-posts-total.log', 'Posts to remove from properties table - ' . count($property_existing_posts_to_remove) . PHP_EOL, FILE_APPEND);
 foreach ($property_existing_posts_to_remove as $post_id) {
     $sql_query = "DELETE FROM `properties` WHERE `post_id` = ?";
     $query = $parsing_db->pdo->prepare($sql_query);
@@ -158,12 +152,11 @@ foreach ($property_existing_posts_to_remove as $post_id) {
 
 
 // 6. Fixing & clearing availability records: bedroom_cnt, bathroom_cnt, listing_price, home_size_sq_ft
-file_put_contents(LOG_DIR . '/remove-posts-total.log', '[' . date('Y-m-d H:i:s') . '] 6. Fixing & clearing availability records: bedroom_cnt, bathroom_cnt, listing_price, home_size_sq_ft' . PHP_EOL, FILE_APPEND);
+file_put_contents(LOG_DIR . '/remove-posts-total.log', PHP_EOL . '[' . date('Y-m-d H:i:s') . '] 6. Fixing & clearing availability records: bedroom_cnt, bathroom_cnt, listing_price, home_size_sq_ft' . PHP_EOL, FILE_APPEND);
 $query = $parsing_db->pdo->prepare("SELECT count(*) FROM `availability` WHERE 1=1 LIMIT 1");
 $query->execute();
 $total_availabilities = $query->fetchColumn();
-echo 'Total availabilities - ' . $total_availabilities . PHP_EOL;
-file_put_contents(LOG_DIR . '/remove-posts-total.log', PHP_EOL . 'Total availabilities - ' . $total_availabilities . PHP_EOL, FILE_APPEND);
+file_put_contents(LOG_DIR . '/remove-posts-total.log', 'Total availabilities - ' . $total_availabilities . PHP_EOL, FILE_APPEND);
 $pages = intdiv($total_availabilities, 100);
 for ($i = 0; $i <= $pages; $i++) {
     $start = $i * 100;
@@ -176,10 +169,10 @@ for ($i = 0; $i <= $pages; $i++) {
         $bath_cnt = $row->bathroom_cnt;
         $listing_price = $row->listing_price;
         $sqft = $row->home_size_sq_ft;
-        $bed_cnt = (trim(preg_replace("/[a-zA-Z]/", "", $bed_cnt)) === '' || trim(preg_replace("/[a-zA-Z]/", "", $bed_cnt)) === NULL) ? 0 : trim(preg_replace("/[a-zA-Z]/", "", $bed_cnt));
-        $bath_cnt = trim(preg_replace("/[a-zA-Z]/", "", $bath_cnt));
-        $listing_price = trim(preg_replace("/[a-zA-Z$,]/", "", $listing_price));
-        $sqft = trim(preg_replace("/[a-zA-Z,]/", "", $sqft));
+        $bed_cnt = (trim(preg_replace("/[a-zA-Z\/]/", "", $bed_cnt)) === '' || trim(preg_replace("/[a-zA-Z\/]/", "", $bed_cnt)) === NULL) ? 0 : trim(preg_replace("/[a-zA-Z]/", "", $bed_cnt));
+        $bath_cnt = trim(preg_replace("/[a-zA-Z\/]/", "", $bath_cnt));
+        $listing_price = trim(preg_replace("/[a-zA-Z$,\/]/", "", $listing_price));
+        $sqft = trim(preg_replace("/[a-zA-Z,\/]/", "", $sqft));
         $query = $parsing_db->pdo->prepare("UPDATE `availability` SET bedroom_cnt = ?, bathroom_cnt = ?, listing_price = ?, home_size_sq_ft = ? WHERE id = ?");
         $query->execute([$bed_cnt, $bath_cnt, $listing_price, $sqft, $row->id]);
     }
@@ -187,12 +180,11 @@ for ($i = 0; $i <= $pages; $i++) {
 
 
 // 7. Removing availability records with post_id IS NULL && NOT Available statuses
-file_put_contents(LOG_DIR . '/remove-posts-total.log', '[' . date('Y-m-d H:i:s') . '] 7. Removing availability records with post_id IS NULL && NOT Available statuses' . PHP_EOL, FILE_APPEND);
+file_put_contents(LOG_DIR . '/remove-posts-total.log', PHP_EOL . '[' . date('Y-m-d H:i:s') . '] 7. Removing availability records with post_id IS NULL && NOT Available statuses' . PHP_EOL, FILE_APPEND);
 $query = $parsing_db->pdo->prepare("SELECT count(*) FROM `availability` WHERE 1=1 LIMIT 1");
 $query->execute();
 $total_availabilities = $query->fetchColumn();
-echo 'Total availabilities - ' . $total_availabilities . PHP_EOL;
-file_put_contents(LOG_DIR . '/remove-posts-total.log', PHP_EOL . 'Total availabilities - ' . $total_availabilities . PHP_EOL, FILE_APPEND);
+file_put_contents(LOG_DIR . '/remove-posts-total.log', 'Total availabilities - ' . $total_availabilities . PHP_EOL, FILE_APPEND);
 $sql_query = "DELETE FROM `availability` WHERE (`post_id` IS NULL AND `status` IS NULL) OR (`post_id` IS NULL AND `status` = '')";
 foreach ($status_availability as $status) {
     $sql_query .= " OR (`post_id` IS NULL AND `status` != '$status')";
@@ -204,12 +196,11 @@ $total_availabilities = $query->fetchColumn();
 
 
 // 8. Removing availability records with post_id IS NULL && WRONG listing_price & WRONG home_size_sq_ft
-file_put_contents(LOG_DIR . '/remove-posts-total.log', '[' . date('Y-m-d H:i:s') . '] 8. Removing availability records with post_id IS NULL && WRONG listing_price & WRONG home_size_sq_ft' . PHP_EOL, FILE_APPEND);
+file_put_contents(LOG_DIR . '/remove-posts-total.log', PHP_EOL . '[' . date('Y-m-d H:i:s') . '] 8. Removing availability records with post_id IS NULL && WRONG listing_price & WRONG home_size_sq_ft' . PHP_EOL, FILE_APPEND);
 $query = $parsing_db->pdo->prepare("SELECT count(*) FROM `availability` WHERE 1=1 LIMIT 1");
 $query->execute();
 $total_availabilities = $query->fetchColumn();
-echo 'Total availabilities - ' . $total_availabilities . PHP_EOL;
-file_put_contents(LOG_DIR . '/remove-posts-total.log', PHP_EOL . 'Total availabilities - ' . $total_availabilities . PHP_EOL, FILE_APPEND);
+file_put_contents(LOG_DIR . '/remove-posts-total.log', 'Total availabilities - ' . $total_availabilities . PHP_EOL, FILE_APPEND);
 $sql_query = "DELETE FROM `availability` WHERE (`post_id` IS NULL AND `listing_price` IS NULL) OR (`post_id` IS NULL AND `listing_price` LIKE '%person%') OR (`post_id` IS NULL AND `listing_price` LIKE '%call%') OR (`post_id` IS NULL AND `home_size_sq_ft` IS NULL) OR (`post_id` IS NULL AND `home_size_sq_ft` = '')";
 file_put_contents(LOG_DIR . '/remove-posts-total.log', $sql_query . PHP_EOL, FILE_APPEND);
 $query = $parsing_db->pdo->prepare($sql_query);
@@ -220,27 +211,25 @@ $total_availabilities = $query->fetchColumn();
 $query = $parsing_db->pdo->prepare("SELECT count(*) FROM `availability` WHERE 1=1 LIMIT 1");
 $query->execute();
 $total_availabilities = $query->fetchColumn();
-echo 'Total availabilities - ' . $total_availabilities . PHP_EOL;
 file_put_contents(LOG_DIR . '/remove-posts-total.log', PHP_EOL . 'Total availabilities - ' . $total_availabilities . PHP_EOL, FILE_APPEND);
 
 
 // 9. Count availability records with WRONG listing_price & WRONG home_size_sq_ft
-file_put_contents(LOG_DIR . '/remove-posts-total.log', '[' . date('Y-m-d H:i:s') . '] 9. Count availability records with WRONG listing_price & WRONG home_size_sq_ft' . PHP_EOL, FILE_APPEND);
-$sql_query = "SELECT count(*) FROM `availability` WHERE `listing_price` IS NULL OR `listing_price` LIKE '%person%' OR `listing_price` LIKE '%call%' OR `home_size_sq_ft` = '' OR `home_size_sq_ft` IS NULL OR `image_urls` = '' OR `image_urls` IS NULL LIMIT 1";
+file_put_contents(LOG_DIR . '/remove-posts-total.log', PHP_EOL . '[' . date('Y-m-d H:i:s') . '] 9. Count availability records with WRONG listing_price & WRONG home_size_sq_ft' . PHP_EOL, FILE_APPEND);
+$sql_query = "SELECT count(*) FROM `availability` WHERE `listing_price` IS NULL OR `listing_price` LIKE '%person%' OR `listing_price` LIKE '%call%' OR `listing_price` = '' OR `home_size_sq_ft` = '' OR `home_size_sq_ft` IS NULL OR `image_urls` = '' OR `image_urls` IS NULL LIMIT 1";
 $query = $parsing_db->pdo->prepare($sql_query);
 $query->execute();
 $total_availabilities = $query->fetchColumn();
-echo 'Total availabilities - ' . $total_availabilities . PHP_EOL;
-file_put_contents(LOG_DIR . '/remove-posts-total.log', PHP_EOL . "Total availability records with WRONG listing_price & WRONG home_size_sq_ft & image_urls  = ' - " . $total_availabilities . PHP_EOL, FILE_APPEND);
+file_put_contents(LOG_DIR . '/remove-posts-total.log', "Total availability records with WRONG listing_price & WRONG home_size_sq_ft & image_urls  = ' - " . $total_availabilities . PHP_EOL, FILE_APPEND);
 
 
 // 10. Getting property_ids && post_ids FROM availability table with post_id = 0 & WRONG listing_price & WRONG home_size_sq_ft & image_urls  = ''
-file_put_contents(LOG_DIR . '/remove-posts-total.log', "[" . date('Y-m-d H:i:s') . "] 10. Getting property_ids && post_ids FROM availability table with post_id = 0 & WRONG listing_price & WRONG home_size_sq_ft & image_urls  = ''" . PHP_EOL, FILE_APPEND);
+file_put_contents(LOG_DIR . '/remove-posts-total.log', PHP_EOL . "[" . date('Y-m-d H:i:s') . "] 10. Getting property_ids && post_ids FROM availability table with post_id = 0 & WRONG listing_price & WRONG home_size_sq_ft & image_urls  = ''" . PHP_EOL, FILE_APPEND);
 $properties_ro_remove = [];
 $pages = intdiv($total_availabilities, 100);
 for ($i = 0; $i <= $pages; $i++) {
     $start = $i * 100;
-    $query = $parsing_db->pdo->prepare("SELECT property_id, post_id FROM `availability` WHERE `post_id` = 0 OR `post_id` = '0' OR `listing_price` IS NULL OR `listing_price` LIKE '%person%' OR `listing_price` LIKE '%call%' OR `home_size_sq_ft` = '' OR `home_size_sq_ft` IS NULL OR `image_urls` = '' OR `image_urls` IS NULL LIMIT $start,100");
+    $query = $parsing_db->pdo->prepare("SELECT property_id, post_id FROM `availability` WHERE `post_id` = 0 OR `post_id` = '0' OR `listing_price` IS NULL OR `listing_price` LIKE '%person%' OR `listing_price` LIKE '%call%' OR `listing_price` = '' OR `home_size_sq_ft` = '' OR `home_size_sq_ft` IS NULL OR `image_urls` = '' OR `image_urls` IS NULL LIMIT $start,100");
     $query->execute();
     $rows = $query->fetchAll(PDO::FETCH_ASSOC);
 
